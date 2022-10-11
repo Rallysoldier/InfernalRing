53ae56e4664a5596eb1aff1f1c08490b650245a2
DMRobbins2000 <dylan.robbins2023@gmail.com>
2022-10-11 12:08:25 -0500

    Functioning intro timer, match timer, and reset feature. needs to be reworked

--- Assets/Scripts/TeamRitual/Core/MatchTimer.cs
+++ Assets/Scripts/TeamRitual/Core/MatchTimer.cs
@@ -3,31 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
+
 
 public class MatchTimer : MonoBehaviour
 {
     float toStartTime = 4f;
     float currentTime = 0f;
-    float matchLength = 180f;
+    float matchLength = 10f;
 
     bool intro;
 
     [SerializeField] TextMeshProUGUI countdownText;
     [SerializeField] TextMeshProUGUI messageText;
+    [SerializeField] TextMeshProUGUI resetText;
 
     void Start()
     {
         intro = true;
         currentTime = toStartTime;
         messageText.text = "";
+        resetText.text = "";
+        Time.timeScale = 1;
     }
 
     void Update()
     {
-        if(intro)
+        if (intro)
         {
+            //To-do: freeze characters until intro is false
             currentTime -= 1 * Time.deltaTime;
-            
+
 
             if (currentTime <= -2)
             {
@@ -44,10 +50,10 @@ public class MatchTimer : MonoBehaviour
                 countdownText.text = currentTime.ToString("0");
             }
         }
-        else if(currentTime > 0)
+        else if (currentTime > 0)
         {
-        currentTime -= 1 * Time.deltaTime;
-        countdownText.text = currentTime.ToString("0");
+            currentTime -= 1 * Time.deltaTime;
+            countdownText.text = currentTime.ToString("0");
         }
         else
         {
@@ -63,6 +69,11 @@ public class MatchTimer : MonoBehaviour
     {
         countdownText.text = "00";
         messageText.text = "GAME OVER";
-        Time.timeScale = 0;
+        Time.timeScale = 0.001F;
+        resetText.text = "Press Esc. to PLAY AGAIN";
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }

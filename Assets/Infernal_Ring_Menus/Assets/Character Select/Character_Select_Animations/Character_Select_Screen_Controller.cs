b607ec524985d37c1557eae4594310c8d89c241e
DMRobbins2000 <dylan.robbins2023@gmail.com>
2022-11-08 12:26:16 -0600

    Char select to menu transition

--- Assets/Infernal_Ring_Menus/Assets/Character Select/Character_Select_Animations/Character_Select_Screen_Controller.cs
+++ Assets/Infernal_Ring_Menus/Assets/Character Select/Character_Select_Animations/Character_Select_Screen_Controller.cs
@@ -9,15 +9,26 @@ using UnityEngine.UI;
 
 public class Character_Select_Screen_Controller : MonoBehaviour
 {
+
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Return))
+        {
+            Application.LoadLevel("GameTest");
+        }        
+    }
+
+    public void ChangeScene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
     }
 }
